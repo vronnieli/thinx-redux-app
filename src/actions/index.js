d89451dcad4d61673b6e86a24462d7e36661afe4
vronnieli <vronnieli@gmail.com>
2016-12-05 15:23:55 -0500

    removed localhost base url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://thinx-challenge-api.herokuapp.com/api/v1/'
-//export const BASE_URL = 'http://localhost:3000/api/v1/'
+
 
 export function fetchPosts(){
   const posts = fetch(`${BASE_URL}posts`).then((response) => {return response.json()}).then((postsPayload) => {return postsPayload})
